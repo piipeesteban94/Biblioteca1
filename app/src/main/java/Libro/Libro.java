a2b7d9a0630807011fc1914c17fdfc404d53dc37
piipeesteban94 <72636710+piipeesteban94@users.noreply.github.com>
2020-10-10 15:04:50 -0300

    se agregaron mas libros al arraylist

--- app/src/main/java/Libro/Libro.java
+++ app/src/main/java/Libro/Libro.java
@@ -5,11 +5,16 @@ public class Libro {
     private int Farenheit;
     private int Revival;
     private int ElAlquimista;
+    private int ElPoder;
+    private int Despertar;
 
     public Libro() {
         Farenheit = 5000;
         Revival = 12000;
         ElAlquimista = 45000;
+        ElPoder = 88000;
+        Despertar = 156000;
+
     }
 
     public int getFarenheit() {
@@ -22,5 +27,11 @@ public class Libro {
 
     public int getAlquimista() {
         return ElAlquimista;
+    }
+    public int getElPoder() {
+        return ElPoder;
+    }
+    public int getDespertar() {
+        return Despertar;
     }
 }
